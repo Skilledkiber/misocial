02ed1c8814e52420a499ce3ecddf1bea3e7efd3d
Skilledkiber <yaroslavvakulenok@gmail.com>
2026-09-19 11:30:22 +0300

    misocial full version + adaptive

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,16 @@
-console.log("Hello, World!");
+const hamburger = document.querySelector ("#hamburger")
+      textTop = document.querySelector ("#text-top")
+
+hamburger.onclick = () => {
+    hamburger.classList.toggle("hamburgerOpen")
+    textTop.classList.toggle("textTopOpen")
+    document.body.classList.toggle("body-mobile")
+}
+
+document.querySelectorAll(".nav-link").forEach(link => {
+    link.onclick = () => {
+        hamburger.classList.remove("hamburgerOpen")
+        textTop.classList.remove("textTopOpen")
+        document.body.classList.remove("body-mobile")
+    }
+})
